e4dbc1d776fcb784d6c56414f804bdbd7e24d036
Chongyu Zhu <lembacon@gmail.com>
2013-01-21 17:58:48 +0800

    Fix initial alpha value for last commit.

--- SquirrelApplicationDelegate.m
+++ SquirrelApplicationDelegate.m
@@ -266,11 +266,10 @@ void notification_handler(void* context_object, RimeSessionId session_id,
   }
   RimeConfigGetInt(config, "style/font_point", &style.fontSize);
   
-  RimeConfigGetDouble(config, "style/alpha", &style.alpha);
-  if (style.alpha > 1.0) {
-    style.alpha = 1.0;
-  } else if (style.alpha < 0.1) {
-    style.alpha = 0.1;
+  double alpha;
+  style.alpha = 1.0;
+  if (RimeConfigGetDouble(config, "style/alpha", &alpha)) {
+    style.alpha = fmax(fmin(alpha, 1.0), 0.1);
   }
   
   RimeConfigGetDouble(config, "style/corner_radius", &style.cornerRadius);
