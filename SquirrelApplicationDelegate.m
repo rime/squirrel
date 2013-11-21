a11b0b11fed95f84914da11e3909c84b736cabeb
LEOYoon-Tsaw <lyc20041@gmail.com>
2013-11-21 18:09:25 +0800

    Add tooltip for extended_charset statue change

--- SquirrelApplicationDelegate.m
+++ SquirrelApplicationDelegate.m
@@ -168,6 +168,10 @@ void notification_handler(void* context_object, RimeSessionId session_id,
 //      show_message(message_value, message_type);
       show_status_message(message_value, message_type);
     }
+    else if (!strcmp(message_value, "extended_charset") || !strcmp(message_value, "!extended_charset")) {
+//      show_message(message_value, message_type);
+      show_status_message(message_value, message_type);
+    }
   }
 }
 
@@ -389,7 +393,7 @@ void notification_handler(void* context_object, RimeSessionId session_id,
     else {
       style.highlightedCandidateBackColor = style.highlightedBackColor;
     }
-    // new in squirrel
+
     [key replaceCharactersInRange:NSMakeRange(prefix_length, [key length] - prefix_length) withString:@"/comment_text_color"];
     if (RimeConfigGetString(config, [key UTF8String], color, sizeof(color))) {
       style.commentTextColor = [NSString stringWithUTF8String:color];
@@ -398,9 +402,6 @@ void notification_handler(void* context_object, RimeSessionId session_id,
     [key replaceCharactersInRange:NSMakeRange(prefix_length, [key length] - prefix_length) withString:@"/hilited_comment_text_color"];
     if (RimeConfigGetString(config, [key UTF8String], color, sizeof(color))) {
       style.highlightedCommentTextColor = [NSString stringWithUTF8String:color];
-    }
-    else {
-      style.highlightedCommentTextColor = style.commentTextColor;
     }
 
     // the following per-color-scheme configurations, if exist, will
