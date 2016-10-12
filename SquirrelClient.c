95227adee811061bbf78ab08ecf6dda97599135f
jixiuf <jixiuf@qq.com>
2016-10-12 14:18:52 +0800

    squirrel_client support --get to get option value

--- SquirrelClient.c
+++ SquirrelClient.c
@@ -11,33 +11,44 @@
 
 char *socket_path="/tmp/squirrel.sock";
 
+#define BUF_LEN 512
 void usage(char **argv){
   printf ("%s\n",argv[0]);
 
+  printf ("get option:\n");
+  printf ("    -g option_name \n");
+  printf ("    --get option_name \n\n");
+
   printf ("set option:\n");
-  printf ("    -s option_name \n\n");
+  printf ("    -s option_name \n");
+  printf ("    --set option_name \n\n");
 
   printf ("unset option:\n");
-  printf ("    -u option_name\n\n");
+  printf ("    -u option_name\n");
+  printf ("    --unset option_name\n\n");
 
   printf ("toggle option:\n");
-  printf ("    -t option_name\n\n");
+  printf ("    -t option_name\n");
+  printf ("    --toggle option_name\n\n");
 
   printf ("clear composition:\n");
   printf ("    -c or --clear\n\n");
 
-  printf("options: ascii_mode,full_shape,ascii_punct,simplification,extended_charset\n");
+  printf("options: ascii_mode,full_shape,ascii_punct,simplification(maybe zh_simp in your schema.yaml),extended_charset\n");
   printf ("demo: -s ascii_mode\n");
   printf ("      -u ascii_mode\n");
   printf ("      -t ascii_mode\n");
   printf ("      -t ascii_mode --clear\n");
+  printf ("      -g ascii_mode -g full_shape\n");
+
+
 
 
 
 }
 
 /* send cmd by unix domain socket to squirrel */
-int sendCmd(char *buf){
+int sendCmd(char *buf,int need_wait_result ,char** output){
   int fd;
   int n;
   struct sockaddr_un un;
@@ -63,19 +74,22 @@ int sendCmd(char *buf){
     close(fd);
     return 3;
   }
+  if(need_wait_result){
+    n=read(fd, *output, BUF_LEN-1) ;
+  }
   /* printf ("write%d\n",n); */
   close(fd);
 
   return 0;
 }
 
-#define BUF_LEN 512
 
-char* const short_options = "s:t:u:c";
+char* const short_options = "g:s:t:u:c";
 struct option long_options[] = {
   { "set",         required_argument,   NULL,    's'     },
   { "toggle",      required_argument,   NULL,    't'     },
   { "unset",       required_argument,   NULL,    'u'     },
+  { "get",       required_argument,   NULL,    'g'     },
   /* { "commit_code", no_argument,         NULL,    'D'     }, */
   /* { "commit_text", no_argument,         NULL,    'T'     }, */
   { "clear",       no_argument,         NULL,    'c'     },
@@ -84,7 +98,9 @@ struct option long_options[] = {
 
 int main(int argc, char **argv) {
   char buf[BUF_LEN];
+  char* output;
   int ch;
+  int need_wait_result=0;
   int handled=0;
 
   opterr = 0;
@@ -92,10 +108,21 @@ int main(int argc, char **argv) {
     usage(argv);
     exit(0);
   }
+  output=malloc(BUF_LEN);
+  memset(output, 0, BUF_LEN);
+
   buf[0]='\0';
   while((ch = getopt_long (argc, argv, short_options, long_options, NULL)) != -1){
     handled=0;
     switch(ch) {
+    case 'g':
+      strcpy(buf+strlen(buf), "--get,");
+      strncpy(buf+strlen(buf), optarg, sizeof(buf)-strlen(buf) - 1);
+      /* printf ("--get: %s\n",buf); */
+      need_wait_result=1;
+      handled=1;
+      break;
+
     case 's':
       strcpy(buf+strlen(buf), "--set,");
       strncpy(buf+strlen(buf), optarg, sizeof(buf)-strlen(buf) - 1);
@@ -143,7 +170,10 @@ int main(int argc, char **argv) {
     strcpy(buf+strlen(buf)-1, "\0"); /* 去除末尾的分隔符 */
     if(strlen(buf)>0){
       /* printf ("%s\n",buf); */
-      sendCmd(buf);
+       sendCmd(buf,need_wait_result,&output);
+       if(strlen(output)!=0){
+         printf ("%s",output);
+       }
     }
   }
 
