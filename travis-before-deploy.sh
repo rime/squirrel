c367b675bbca4f7e4467b71b9f42adbb888b77a5
Chen Gong <chen.sst@gmail.com>
2019-01-22 23:10:44 +0800

    feat(travis-ci): deploy release package
    trigger custom build on travis-ci with YAML config
    before_deploy: package=release publish=false ./travis-before-deploy.sh

--- travis-before-deploy.sh
+++ travis-before-deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+package_name="${package:-testing}"
+publish="${publish:-true}"
 get_app_version() {
     sed -n '/CFBundleVersion/{n;s/.*<string>\(.*\)<\/string>.*/\1/;p;}' $@
 }
 app_version="$(get_app_version 'Info.plist')"
-git_hash="$(git rev-parse HEAD | cut -c -7)"
-built_archive="package/archives/Squirrel-${app_version}.zip"
-upload_archive="package/archives/Squirrel-${app_version}+git${git_hash}.zip"
-mv "${built_archive}" "${upload_archive}"
-sed -i '.bak' "{
-  s/{{app_version}}/${app_version}/g
-  s/{{git_hash}}/${git_hash}/g
-}" travis-deploy-package.json
+version_name="${app_version}"
+version_desc="鼠鬚管 ${version_name}"
+if [[ "${package_name}" = 'testing' ]]
+then
+    git_hash="$(git rev-parse HEAD | cut -c -7)"
+    version_name="${app_version}+git${git_hash}"
+    version_desc="鼠鬚管測試版 ${version_name}"
+    built_archive="package/archives/Squirrel-${app_version}.zip"
+    upload_archive="package/archives/Squirrel-${version_name}.zip"
+    mv "${built_archive}" "${upload_archive}"
+fi
+sed "{
+  s/{{package_name}}/${package_name}/g
+  s/{{version_name}}/${version_name}/g
+  s/{{version_desc}}/${version_desc}/g
+  s/{{publish}}/${publish}/g
+}" travis-deploy-package.json.template > travis-deploy-package.json
