d7bbceba53ef618d563a11b458e3ea8b7aca578b
LEOYoon-Tsaw <leoyoontsaw@icloud.com>
2021-10-01 17:41:20 -0400

    Optimize code

--- SquirrelPanel.m
+++ SquirrelPanel.m
@@ -897,7 +897,7 @@ void changeEmojiSize(NSMutableAttributedString *text, CGFloat emojiFontSize) {
     if (@available(macOS 10.14, *)) {
       _back = [[NSVisualEffectView alloc] init];
       _back.blendingMode = NSVisualEffectBlendingModeBehindWindow;
-      _back.material = NSVisualEffectMaterialFullScreenUI;
+      _back.material = NSVisualEffectMaterialHUDWindow;
       _back.state = NSVisualEffectStateActive;
       _back.wantsLayer = YES;
       _back.layer.mask = _view.shape;
@@ -1010,35 +1010,23 @@ void changeEmojiSize(NSMutableAttributedString *text, CGFloat emojiFontSize) {
     windowRect.origin.y = NSMinY(_screenRect);
   }
   [self setFrame:windowRect display:YES];
+  // rotate the view, the core in vertical mode!
+  if (theme.vertical) {
+    self.contentView.boundsRotation = -90.0;
+    [self.contentView setBoundsOrigin:NSMakePoint(0, windowRect.size.width)];
+  } else {
+    self.contentView.boundsRotation = 0;
+    [self.contentView setBoundsOrigin:NSMakePoint(0, 0)];
+  }
   BOOL translucency = theme.translucency;
-  [_view setFrame:_view.superview.bounds];
+  [_view setFrame:self.contentView.bounds];
   if (@available(macOS 10.14, *)) {
     if (translucency) {
-      [_back setFrame:_back.superview.bounds];
+      [_back setFrame:self.contentView.bounds];
       _back.appearance = NSApp.effectiveAppearance;
       [_back setHidden:NO];
     } else {
       [_back setHidden:YES];
-    }
-  }
-  // rotate the view, the core in vertical mode!
-  if (theme.vertical) {
-    _view.boundsRotation = 90.0;
-    [_view setBoundsOrigin:NSMakePoint(0, windowRect.size.width)];
-    if (@available(macOS 10.14, *)) {
-      if (translucency) {
-        _back.boundsRotation = 90.0;
-        [_back setBoundsOrigin:NSMakePoint(0, windowRect.size.width)];
-      }
-    }
-  } else {
-    _view.boundsRotation = 0;
-    [_view setBoundsOrigin:NSMakePoint(0, 0)];
-    if (@available(macOS 10.14, *)) {
-      if (translucency) {
-        _back.boundsRotation = 0;
-        [_back setBoundsOrigin:NSMakePoint(0, 0)];
-      }
     }
   }
   self.alphaValue = theme.alpha;
