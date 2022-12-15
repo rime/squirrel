ce68080057f954aedbe76d901bdfac044daaf15f
groverlynn <groverlynn@gmail.com>
2022-12-15 03:08:26 +0100

    Distinguish Left/Right Modifiers

--- SquirrelInputController.m
+++ SquirrelInputController.m
@@ -79,6 +79,7 @@ const int N_KEY_ROLL_OVER = 50;
           break;
         }
         //NSLog(@"FLAGSCHANGED client: %@, modifiers: 0x%lx", sender, modifiers);
+        int keyCode = event.keyCode;
         int rime_modifiers = osx_modifiers_to_rime_modifiers(modifiers);
         int release_mask = 0;
         NSUInteger changes = _lastModifier ^ modifiers;
@@ -91,22 +92,35 @@ const int N_KEY_ROLL_OVER = 50;
         }
         if (changes & OSX_SHIFT_MASK) {
           release_mask = modifiers & OSX_SHIFT_MASK ? 0 : kReleaseMask;
-          [self processKey:XK_Shift_L
-                 modifiers:(rime_modifiers | release_mask)];
+          if (keyCode == 0x38) {
+            [self processKey:XK_Shift_L modifiers:(rime_modifiers | release_mask)];
+          } else if (keyCode == 0x3c) {
+            [self processKey:XK_Shift_R modifiers:(rime_modifiers | release_mask)];
+          }
         }
         if (changes & OSX_CTRL_MASK) {
           release_mask = modifiers & OSX_CTRL_MASK ? 0 : kReleaseMask;
-          [self processKey:XK_Control_L
-                 modifiers:(rime_modifiers | release_mask)];
+          if (keyCode == 0x3b) {
+            [self processKey:XK_Control_L modifiers:(rime_modifiers | release_mask)];
+          } else if (keyCode == 0x3e) {
+            [self processKey:XK_Control_R modifiers:(rime_modifiers | release_mask)];
+          }
         }
         if (changes & OSX_ALT_MASK) {
           release_mask = modifiers & OSX_ALT_MASK ? 0 : kReleaseMask;
-          [self processKey:XK_Alt_L modifiers:(rime_modifiers | release_mask)];
+          if (keyCode == 0x3a) {
+            [self processKey:XK_Alt_L modifiers:(rime_modifiers | release_mask)];
+          } else if (keyCode == 0x3d) {
+            [self processKey:XK_Alt_R modifiers:(rime_modifiers | release_mask)];
+          }
         }
         if (changes & OSX_COMMAND_MASK) {
           release_mask = modifiers & OSX_COMMAND_MASK ? 0 : kReleaseMask;
-          [self processKey:XK_Super_L
-                 modifiers:(rime_modifiers | release_mask)];
+          if (keyCode == 0x37) {
+            [self processKey:XK_Super_L modifiers:(rime_modifiers | release_mask)];
+          } else if (keyCode == 0x36) {
+            [self processKey:XK_Super_R modifiers:(rime_modifiers | release_mask)];
+          }
           // do not update UI when using Command key
           break;
         }
