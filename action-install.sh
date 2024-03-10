b4f8f58161c75c5615a4280658974b8eb49b02fa
groverlynn <groverlynn@gmail.com>
2024-03-10 19:09:03 +0100

    use latest librime build
    Update librime

--- action-install.sh
+++ action-install.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-rime_version=1.10.0
-rime_git_hash=295cb2a
+rime_version=1.11.0
+rime_git_hash=76a0a16
 
-rime_archive="rime-${rime_git_hash}-macOS.tar.bz2"
+rime_archive="rime-${rime_git_hash}-macOS-universal.tar.bz2"
 rime_download_url="https://github.com/rime/librime/releases/download/${rime_version}/${rime_archive}"
 
-rime_deps_archive="rime-deps-${rime_git_hash}-macOS.tar.bz2"
+rime_deps_archive="rime-deps-${rime_git_hash}-macOS-universal.tar.bz2"
 rime_deps_download_url="https://github.com/rime/librime/releases/download/${rime_version}/${rime_deps_archive}"
 
 mkdir -p download && (
