413033547f5b56280eeb80a4f0cf856e325e8495
LEO Yoon-Tsaw <leoyoontsaw@icloud.com>
2024-05-13 21:28:45 -0400

    lint

--- Squirrel-Bridging-Header.h
+++ Squirrel-Bridging-Header.h
@@ -1,5 +1,6 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  Use this file to import your target's public headers that you would like to
+//  expose to Swift.
 //
 
 #define STDBOOL
