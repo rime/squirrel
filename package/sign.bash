7f7ed6b4eabbaef8390f3c20145ccf333aadc197
LEO Yoon-Tsaw <leoyoontsaw@icloud.com>
2024-05-18 16:34:42 -0400

    improve codesign

--- package/sign.bash
+++ package/sign.bash
@@ -1,16 +1,6 @@
 appDir="build/Release/Squirrel.app"
 entitlement="resources/Squirrel.entitlements"
 
-allFiles=$(find "$appDir" -print)
-for file in $allFiles; do
-	if ! [[ -d "$file" ]]; then
-		if [[ -x "$file" ]]; then
-			echo "$file"
-			codesign --force --entitlements "$entitlement" --options runtime --timestamp --sign "Developer ID Application: $1" "$file";
-		fi;
-	fi;
-done;
-
-codesign -d --entitlements --entitlements "$entitlement" --force --options runtime --timestamp --sign "Developer ID Application: $1" "$appDir";
+codesign --deep --force --options runtime --timestamp --sign "Developer ID Application: $1" --entitlements "$entitlement" --verbose "$appDir";
 
 spctl -a -vv "$appDir";
