e2570344d6ec44c38d65b493737381521885e6a6
LEO Yoon-Tsaw <leoyoontsaw@icloud.com>
2024-05-26 22:40:39 -0400

    [CI] Use prebuilt sparkle for CI
    Update Sparkle to 2.6.2, aligned with downloadable tag

--- action-install.sh
+++ action-install.sh
@@ -4,6 +4,7 @@ set -e
 
 rime_version=latest
 rime_git_hash=6b1b41f
+sparkle_version=2.6.2
 
 rime_archive="rime-${rime_git_hash}-macOS-universal.tar.bz2"
 rime_download_url="https://github.com/rime/librime/releases/download/${rime_version}/${rime_archive}"
@@ -11,17 +12,24 @@ rime_download_url="https://github.com/rime/librime/releases/download/${rime_vers
 rime_deps_archive="rime-deps-${rime_git_hash}-macOS-universal.tar.bz2"
 rime_deps_download_url="https://github.com/rime/librime/releases/download/${rime_version}/${rime_deps_archive}"
 
+sparkle_archive="Sparkle-${sparkle_version}.tar.xz"
+sparkle_download_url="https://github.com/sparkle-project/Sparkle/releases/download/${sparkle_version}/${sparkle_archive}"
+
 mkdir -p download && (
     cd download
     [ -z "${no_download}" ] && curl -LO "${rime_download_url}"
     tar --bzip2 -xf "${rime_archive}"
     [ -z "${no_download}" ] && curl -LO "${rime_deps_download_url}"
     tar --bzip2 -xf "${rime_deps_archive}"
+    [ -z "${no_download}" ] && curl -LO "${sparkle_download_url}"
+    tar -xJf "${sparkle_archive}"
 )
 
 mkdir -p librime/share
+mkdir -p Frameworks
 cp -R download/dist librime/
 cp -R download/share/opencc librime/share/
+cp -R download/Sparkle.framework Frameworks/
 
 # skip building librime and opencc-data; use downloaded artifacts
 make copy-rime-binaries copy-opencc-data
